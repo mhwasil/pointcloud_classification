e650bfd97d417a86c70f35dc117594591c064ed8
Mohammad Wasil <mwasil@outlook.co.id>
2021-05-09 18:14:43 +0200

    rename job name and fix path

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=train-3dmfv-net-tf
+#SBATCH --job-name=train-pc-cls
 #SBATCH --partition=gpu
 #SBATCH --nodes=1                       # number of nodes
 #SBATCH --mem=64GB                      # memory per node in MB (different units with$
@@ -9,13 +9,13 @@
 #SBATCH --error log/job_tf.%N.%j.err    # filename for STDERR
 
 # load cuda
-module load cuda
+module load cuda/9.2
 
 # activate environment
-source ~/anaconda3/bin/activate ~/anaconda3/envs/env-pointcloud
+source ~/anaconda3/bin/activate ~/anaconda3/envs/pc-classification
 
 # locate to your root directory 
-cd /home/mwasil2s/deeplearning_walkthrough/3DmFV-Inception
+cd /home/mwasil2s/deeplearning_walkthrough/pointcloud_classification
 
 # run the script
 python train_fvnet_inception.py --dataset_name=rgbd_washington --num_gaussians=12 --fvnet=0 --num_inception=5 --normalize=1
